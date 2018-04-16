1d5062bf587aec5d3ccf66e450fbf47c5e42d196
EvanChiou <35692086+EvanChiou@users.noreply.github.com>
2018-04-16 18:18:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Game = Engine('stage');
-for(var GameAttr in Game){
+for (var GameAttr in Game) {
     window[GameAttr] = Game[GameAttr]
 }
 
@@ -9,51 +9,26 @@ Game.set({
     debugMode: true // Default: false
 });
 
-Game.preload([],function() {
+Game.preload([
+    "./r3-1.png", " ./陶器碎片-1.png ",
+    "./開門16.png", "  ./開門15.png", " ./開門14.png", "  ./開門13.png ", " ./開門12.png ", " ./開門11.png ", " ./開門10.png ",
+    "./開門9.png ", " ./開門8.png ", " ./開門7.png ", " ./開門6.png ", " ./開門5.png", "  ./開門4.png ", " ./開門3.png ", " ./開門2.png ",
+    "./開門1-3.png", "  ./開門1-2.png", "  ./開門1-1.png", "  ./閉門-1.png ", " ./閉門.png ", " ./神像3-有.png ", " ./神像3-器-1.png ", " ./神像3-器.png ",
+    "./密碼.png ", " ./寶箱底圖.png ", " ./寶箱大圖1.png ", " ./寶箱大圖(開).png ", " ./寶箱小圖101.png ", " ./數-草紙.png ", " ./數-題目.png ",
+    "./數-卷軸-1.png ", " ./數-卷軸.png ", " ./數-格.jpg ", " ./木乃伊(開.png ", " ./藥水-1.png ", " ./189.png ", " ./神像2-有.png ",
+    "./神像2-器-1.png", "  ./神像2-器.png", "  ./生命之符-點取位置.png", "  ./r2-1(有火把)-1光束.png ",
+    "./eye3-黃色路線2.png ", " ./eye1透明路線.png", " ./888.png ", " ./生命之符-點取位置.png ", " ./587925.png ",
+    "./寶石-斜.png ", " ./寶石-正-1.png ", " ./寶石-正.png ", " ./陶罐&斧頭-6.png ", " ./陶罐&斧頭-5.png ", " ./陶罐&斧頭-4.png",
+    "./陶罐&斧頭-3.png", "  ./陶罐&斧頭-2.png ", " ./陶罐&斧頭-1.png", "  ./陶罐底圖-1.png", "  ./陶罐.png ", " ./神像1-器-1.png",
+    "./神像1-無.png ", " ./神像1-有.png", "  ./藥水.png", "  ./木乃伊(閉).png ", " ./5.png ", " ./r2-1mummy.png ", " ./神像4-器-1.png ",
+    "./神像4-器.png ", " ./神像4-無.png", " ./神像4-有.png ", " ./神像1-器.png ", " ./神像3-無.png", "  ./神像2-無.png ", " ./鳥.png 蛇.png",
+    "./眼睛.png", "  ./十字.png", " ./寶箱大圖打開.png ", " ./寶箱底圖-1.png ", " ./寶箱大圖.png ", " ./寶箱小圖.png", "  ./書(闔起).png",
+    "./象形文字紙捲打開.png", " 887.png", "  ./書(開).png ", " ./火把2.png ", " ./r-2.png ", " ./暗格2.jpg ", " ./象形文字紙捲.png",
+    "./火把.png ", " ./火爐大圖.png", " ./r1-1-1.png ", " ./火爐小圖.png", " ./886.png ", " ./牆壁.png", "  ./火把小圖.png ",
+    "./XX.png", "  ./555.png", "  ./壁.png ", " ./暗格1.jpg", "  ./畫2.jpg", " ./素材.png", "  ./859.png ", " ./96.png ", " ./88.png ", " ./998.png"
+], function () {
     Game.start();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 alert("箭頭切換房間，並蒐集所需的物品(各神像的法器)，來逃出密室。")
 var lllooolll = 0
 var lllooolll2 = 0
@@ -88,10 +63,10 @@ var r = createSprite({
         , "書(開).png", "寶箱底圖-1.png", "5.png", "陶罐底圖-1.png", "陶罐&斧頭-1.png "
         , "陶罐&斧頭-2.png", " 陶罐&斧頭-3.png", "陶罐&斧頭-4.png", "陶罐&斧頭-5.png"
         , "陶罐&斧頭-6.png", "eye1透明路線.png", "eye3-黃色路線2.png", "r2-1(有火把)-1光束.png",
-        "數-格.jpg", "寶箱底圖.png", "閉門.png"," 閉門-1.png"," 開門1-1.png"," 開門1-2.png",
-        "開門1-3.png","開門2.png"," 開門3.png"," 開門4.png"," 開門5.png"," 開門6.png", 
-        "開門7.png","開門8.png"," 開門9.png","開門10.png"," 開門11.png"," 開門12.png",
-        "開門13.png"," 開門14.png"," 開門15.png"," 開門16.png"]
+        "數-格.jpg", "寶箱底圖.png", "閉門.png", " 閉門-1.png", " 開門1-1.png", " 開門1-2.png",
+        "開門1-3.png", "開門2.png", " 開門3.png", " 開門4.png", " 開門5.png", " 開門6.png",
+        "開門7.png", "開門8.png", " 開門9.png", "開門10.png", " 開門11.png", " 開門12.png",
+        "開門13.png", " 開門14.png", " 開門15.png", " ./開門16.png"]
 });
 var way1 = createSprite({
     x: 600,
@@ -472,52 +447,52 @@ forever(function () {
         cou.hidden = true
         tt.hidden = true
         way1.hidden = true
-        r.costumeId  = 21
-        setTimeout(function(){
+        r.costumeId = 21
+        setTimeout(function () {
             r.costumeId = 22
-        },1000)
-        setTimeout(function(){
+        }, 1000)
+        setTimeout(function () {
             r.costumeId = 23
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 24
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 25
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 26
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 27
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 28
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 29
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 30
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 31
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 32
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 33
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 34
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 35
-        },2)
-        setTimeout(function(){
+        }, 2)
+        setTimeout(function () {
             r.costumeId = 36
-        },2)
+        }, 2)
     }
 });
 when("click", function () {
