e8df66d8017133ad5beb4be87321aa8c68f32faf
EvanChiou <35692086+EvanChiou@users.noreply.github.com>
2018-05-09 19:04:15 +0800

    第二關bug的發現與修正前的準備

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("成功 bang~")
+alert("成功")
 var Game = Engine('stage');
 for (var GameAttr in Game) {
     window[GameAttr] = Game[GameAttr]
@@ -139,6 +139,13 @@ function level_2() {
     var text_4 = '';
     var text_5 = '';
     var text_6 = '';
+    var playing = createSprite({
+        x: 320,
+        y: 250,
+        scale: 0.8,
+        costumes: ["2~3.png"],
+        layer: 1000000
+    })
     var q1 = createSprite({
         x: 200,
         y: 100,
@@ -181,25 +188,35 @@ function level_2() {
         costumes: ["100000.png"],
         hidden: true
     });
+    playing.hidden = true
     var stair = 0;
-    sprites = [q1, q2, q3, q4, q5, q6]
+    sprites = [playing, q1, q2, q3, q4, q5, q6]
     forever(function () {
         if (level != 2) {
             return
         }
         if (text_1 == 1 && text_2 == 10 && text_3 == 100 && text_4 == 1000 && text_5 == 10000 && text_6 == 100000) {
-            alert("恭喜過關!!")
-            setBackdrop("white")
-            next_level();
+            playing.hidden = false
+            stair = 2
         }
-        print(text_1, q1.x - 15, q1.y + 35, "white", 45);
-        print(text_2, q2.x - 25, q2.y + 35, "white", 45);
-        print(text_3, q3.x - 40, q3.y + 35, "white", 45);
-        print(text_4, q4.x - 50, q4.y + 35, "white", 45);
-        print(text_5, q5.x - 60, q5.y + 35, "white", 45);
-        print(text_6, q6.x - 70, q6.y + 35, "white", 45);
+        if(stair == 1){
+            print(text_1, q1.x - 15, q1.y + 35, "white", 45);
+            print(text_2, q2.x - 25, q2.y + 35, "white", 45);
+            print(text_3, q3.x - 40, q3.y + 35, "white", 45);
+            print(text_4, q4.x - 50, q4.y + 35, "white", 45);
+            print(text_5, q5.x - 60, q5.y + 35, "white", 45);
+            print(text_6, q6.x - 70, q6.y + 35, "white", 45);
+        }
     });
-
+    when("click", function () {
+        if(level == 3){
+            return
+        }
+        if (text_1 == 1 && text_2 == 10 && text_3 == 100 && text_4 == 1000 && text_5 == 10000 && text_6 == 100000) {
+            setBackdrop("white")
+            next_level()
+        }
+    })
     when("click", function () {
         if (stair == 1) {
             return
